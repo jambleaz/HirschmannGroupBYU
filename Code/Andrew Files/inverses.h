98a592b644e7a570caf5d839bb4c10d28d5b8777
James Bleazard <jambleaz@gmail.com>
2024-06-27 15:34:09 -0600

    Started working on finding the inverses.

---
+++ Code/Andrew Files/inverses.h
@@ -0,0 +1,36 @@
+#include <cmath>
+#include<iostream>
+
+using namespace std;
+
+double* inverseMbar(int n, double h) {
+    
+    double* Mbar = new double[n * n]();
+
+    for (int i = 0; i < ceiling(n/2); i++) {
+        for (int j = 0; j < n; j++) {
+            if() /*eta^2 < 0*/ {
+               if (i==1 && j == 2) {
+                    Mbar[i * n + j] = g^2*sin((k-5)*g)               } else if (i == 1 && j >= 3) {
+
+               } else if (i == 2 && j >= 3) {
+
+               } else {
+
+               }
+
+            } else /*eta^2 >= 0*/ {
+                if (i==1 && j == 2) {
+
+               } else if (i == 1 && j >= 3) {
+
+               } else if (i == 2 && j >= 3) {
+
+               } else {
+                
+               }
+            }
+        }
+    }
+
+}
